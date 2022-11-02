0440ad74cba58fb0fb573c1ffb268031986b982d
Giulliano <llthguimaraes@gmail.com>
2022-11-01 21:55:56 -0300

    Filtro de tarefas - todos, ativos e completos

---
+++ scripts/filter.js
@@ -0,0 +1,65 @@
+const task = document.querySelectorAll(".j_task");
+const list = document.querySelector(".j_list");
+let filter = [];
+
+const filterAll = document.querySelector(".j_filter_all");
+const filterActive = document.querySelector(".j_filter_active");
+const filterCompleted = document.querySelector(".j_filter_completed");
+
+const updateList = (tasks) => {
+    list.innerHTML = "";
+    tasks.forEach((item) => {
+        list.append(item);
+    })
+}
+
+const showAll = () => {
+    updateList(task);
+}
+
+const showActive = () => {
+    filter = [];
+    task.forEach(item => {
+        if (!item.classList.contains("completed")) {
+            filter.push(item);
+        }
+    });
+
+    updateList(filter);
+}
+
+const showCompleted = () => {
+    filter = [];
+    task.forEach(item => {
+        if (item.classList.contains("completed")) {
+            filter.push(item);
+        }
+    });
+
+    updateList(filter);
+}
+
+const filterTask = () => {
+    filterAll.addEventListener("click", () => {
+        showAll();
+        filterAll.classList.add("active");
+        filterActive.classList.remove("active");
+        filterCompleted.classList.remove("active");
+    });
+
+    filterActive.addEventListener("click", () => {
+        showActive();
+        filterAll.classList.remove("active");
+        filterActive.classList.add("active");
+        filterCompleted.classList.remove("active");
+    });
+
+    filterCompleted.addEventListener("click", () => {
+        showCompleted();
+        filterAll.classList.remove("active");
+        filterActive.classList.remove("active");
+        filterCompleted.classList.add("active");
+    });
+}
+
+export default filterTask;
--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 import completeTask from "./complete.js";
 import deleteTask from "./delete.js";
+import filterTask from "./filter.js";
 import changeTheme from "./theme.js";
 
 changeTheme();
 completeTask();
 deleteTask();
+filterTask();
